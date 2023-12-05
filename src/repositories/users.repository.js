16ebb3d742e65f80779af54e91e5389afa473e5d
minimini1212 <zjonyv12@naver.com>
2023-12-06 03:27:39 +0900

    feat: 회원가입 api 생성

---
+++ src/repositories/users.repository.js
@@ -0,0 +1,26 @@
+import { prisma } from '../utils/prisma/index.js';
+
+export class UsersRepository {
+  //회원가입
+  createUser = async (email, name, password) => {
+    const user = await prisma.users.create({
+      data: {
+        email,
+        name,
+        password,
+      },
+    });
+    return user;
+  };
+
+
+  //유저 찾기
+  findUser = async(email, name) => {
+    const existUser = await prisma.users.findFirst({
+      where: {
+        OR: [{ name }, { email }],
+      },
+    });
+    return existUser;
+  }
+}
